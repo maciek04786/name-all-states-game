0fd7207501247c26c75d9ae4ed9402539d1c8f4d
MSikora <105375946+maciek04786@users.noreply.github.com>
2022-05-25 20:07:20 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.setup(width=800, height=600)
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+states_data = pandas.read_csv("50_states.csv")
+
+writer = turtle.Turtle()
+writer.hideturtle()
+writer.penup()
+
+all_states = states_data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    screen.title(f"{len(guessed_states)}/50 States Correct")
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        break
+    if states_data.state.str.contains(answer_state).any():
+        answer_x = float(states_data[states_data.state == answer_state]["x"])
+        answer_y = float(states_data[states_data.state == answer_state]["y"])
+        writer.goto(answer_x, answer_y)
+        writer.write(answer_state)
+        guessed_states.append(answer_state)
+
+remaining_states = []
+
+for state in all_states:
+    if state not in guessed_states:
+        remaining_states.append(state)
+
+data = pandas.DataFrame(remaining_states)
+df = data.to_csv("remaining_states.csv")
